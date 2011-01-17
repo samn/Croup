76e3d451cd324b8fd59aa24c30d1dd9d917ca436
sam neubardt <samneubardt@gmail.com>
2011-01-16 20:09:01 -0500

    Test cleanup/refactoring.

--- test/core.js
+++ test/core.js
@@ -1,30 +1,30 @@
-describe('class construction', function() {
+describe('ClassContainer', function() {
     it('Croup.these(...) should return a ClassContainer', function() {
         var cc = Croup.these('ClassA', 'ClassB');
         expect(cc.classNames).toBeDefined();
         expect(cc.have).toBeDefined();
     });
+});
 
-    describe('class construction', function() {
-        var props = {
-            methodA: function() { return 'methodA'; },
-            propertyA: 'propertyA'
-        };
-        Croup.these('ClassA', 'ClassB').have(props);
+describe('Class construction', function() {
+    var props = {
+        methodA: function() { return 'methodA'; },
+        propertyA: 'propertyA'
+    };
+    Croup.these('ClassA', 'ClassB').have(props);
 
-        it('classes should be created', function() {
-            expect(ClassA).toBeDefined();
-            expect(ClassB).toBeDefined();
-        });
+    it('classes should be created', function() {
+        expect(ClassA).toBeDefined();
+        expect(ClassB).toBeDefined();
+    });
 
-        it('classes should have properties and methods defined', function() {
-            ca = new ClassA()
-            expect(ca.methodA()).toEqual('methodA');
-            expect(ca.propertyA).toEqual('propertyA');
+    it('classes should have properties and methods defined', function() {
+        ca = new ClassA()
+        expect(ca.methodA()).toEqual('methodA');
+        expect(ca.propertyA).toEqual('propertyA');
 
-            cb = new ClassB()
-            expect(cb.methodA()).toEqual('methodA');
-            expect(cb.propertyA).toEqual('propertyA');
-        });
+        cb = new ClassB()
+        expect(cb.methodA()).toEqual('methodA');
+        expect(cb.propertyA).toEqual('propertyA');
     });
 });
